df03ae57e3a40eaa99668a32861665b0a10d69d1
Arbind <ak3636869.ak@gmail.com>
2024-02-27 16:35:23 +0530

    done

--- src/assignment/Assignment47.java
+++ src/assignment/Assignment47.java
@@ -1,9 +1,12 @@
 package assignment;
 
+import java.util.Scanner;
+
 public class Assignment47 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
 
 	}
 
