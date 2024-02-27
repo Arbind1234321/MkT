2d4c072ba654d17a6894303d8e2192a54f912213
Arbind <ak3636869.ak@gmail.com>
2024-02-27 19:39:37 +0530

    type casting example

---
+++ src/javabasic/Student.java
@@ -0,0 +1,15 @@
+package javabasic;
+class Teacher{
+	
+}
+class mentor extends Teacher{
+	
+}
+public class Student extends mentor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
